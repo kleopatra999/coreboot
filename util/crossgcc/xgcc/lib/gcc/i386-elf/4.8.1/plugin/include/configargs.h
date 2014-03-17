d114a56350bbe629e874cc3c0acd993b7c6fd515
hongjinkun <hjk@rock-chips.com>
2014-03-17 00:36:23 -0700

    add crossgcc

---
+++ util/crossgcc/xgcc/lib/gcc/i386-elf/4.8.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.8.1/configure --prefix=/home/hongjinkun/share/ddisk/coreboot/coreboot/util/crossgcc/xgcc --libexecdir=/home/hongjinkun/share/ddisk/coreboot/coreboot/util/crossgcc/xgcc/lib --target=i386-elf --disable-werror --disable-shared --disable-libssp --disable-bootstrap --disable-nls --disable-libquadmath --without-headers --enable-lto --enable-languages=c --with-gmp=/home/hongjinkun/share/ddisk/coreboot/coreboot/util/crossgcc/xgcc --with-mpfr=/home/hongjinkun/share/ddisk/coreboot/coreboot/util/crossgcc/xgcc --with-mpc=/home/hongjinkun/share/ddisk/coreboot/coreboot/util/crossgcc/xgcc --with-libelf=/home/hongjinkun/share/ddisk/coreboot/coreboot/util/crossgcc/xgcc --with-pkgversion='coreboot toolchain v1.20 December 4th, 2012'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "i386" }, { "arch", "i386" } };
