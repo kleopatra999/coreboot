67596df024fe2c4a8cc28c0303dd07d56e4df3a5
hl <hl@rock-chips.com>
2014-05-22 18:12:28 +0800

    update config.rksdk

--- write_coreboot_spi.sh
+++ write_coreboot_spi.sh
@@ -1,3 +1,3 @@
 ./Linux_Upgrade_Tool_v1.17/upgrade_tool db Linux_Upgrade_Tool_v1.17/RK32Boot.bin
-./Linux_Upgrade_Tool_v1.17/upgrade_tool wl 0 build/coreboot.rom
+./Linux_Upgrade_Tool_v1.17/upgrade_tool wl 0 image.bin
 
